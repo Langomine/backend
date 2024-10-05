3144e653eb8e91b5a775704109e8f37f4dc6512e
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2024-10-05 18:35:03 +0330

    WIP

--- api/tests/test_setup.py
+++ api/tests/test_setup.py
@@ -1,5 +1,11 @@
+from django.test import override_settings
 from rest_framework.test import APITestCase
 
+@override_settings(STORAGES={
+    "default": {
+        "BACKEND": "django.core.files.storage.memory.InMemoryStorage",
+    },
+})
 class TestSetUp(APITestCase):
     def setUp(self):
         return super().setUp()
